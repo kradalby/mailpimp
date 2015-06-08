24cc8ca620bdfbff9fbc1620cc0b3802e299c169
Kristoffer Dalby <kradalby@kradalby.no>
2015-06-08 23:52:05 +0200

    Derp

--- list.py
+++ list.py
@@ -1,3 +1,7 @@
+import logging
+
+logging.basicConfig(filename="/var/log/mailpimp.log", level=logging.DEBUG)
+logger = logging.getLogger('MailPimpList')
 
 class ListManager():
     def __init__(self, list_file):
@@ -7,12 +11,15 @@ class ListManager():
             for list_string in lists:
                 list = List(list_string)
                 self.lists[list.name] = list
-            print(self.lists)
+                logger.debug("Added list %s" % list.get_name())
 
     def get_list(self, name):
         if name in self.lists.keys():
             return self.lists[name]
         return None
+
+    def get_lists(self):
+        return self.lists
 
 
 class List():
--- mailpimp.py
+++ mailpimp.py
@@ -2,43 +2,54 @@
 
 import email
 import sys
+import os
 import logging
 import configparser
 
 from list import ListManager
 
 logging.basicConfig(filename="/var/log/mailpimp.log", level=logging.DEBUG)
-logger = logging.getLogger('mailpimp')
+logger = logging.getLogger('MailPimp')
+
+CONFIG_FILE = os.path.dirname(os.path.abspath(__file__)) + "/" + "config.ini"
 
 
 class MailPimp():
     def __init__(self, sender, recipient, mail):
         self.config = configparser.ConfigParser()
-        self.config.read("config.ini")
+        self.config.read(CONFIG_FILE)
         self.lm = ListManager(self.config["list"]["list_file"])
+        logger.debug(self.lm.get_lists())
 
         self.sender = sender
         self.recipient = recipient
         self.mail = mail
 
-    def allowed(self, recipient, sender):
-        list = self.lm.get_list(recipient)
-        if list and sender in list.get_senders():
+    def allowed(self):
+        list = self.lm.get_list(self.recipient)
+        if list and self.sender in list.get_senders():
             return True
         return False
 
     def distribute(self):
-        if self.allowed(self.recipient, self.sender):
+        if self.allowed():
             logger.info("Sender %s, is authorized to send to %s" %
                         (self.sender, self.recipient))
+        else:
+            logger.info("Sender %s, is not authorized to send to %s" %
+                        (self.sender, self.recipient))
+
 
 if __name__ == '__main__':
     try:
         mail = email.message_from_binary_file(sys.stdin.buffer)
-        sender = sys.argv[0]
-        recipient = sys.argv[1]
+        sender = sys.argv[1]
+        recipient = sys.argv[2]
+
+        logger.debug("To: %s, From: %s" % (recipient, sender))
 
         mp = MailPimp(sender, recipient, mail)
+        mp.distribute()
     except Exception as e:
         logger.exception(e)
         sys.exit(1)
