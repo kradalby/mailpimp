1593df557010cd570bda435070725e05ca8a46e7
Kristoffer Dalby <kradalby@kradalby.no>
2015-06-08 20:54:26 +0200

    Whatever will be, will be 8{

--- mailpimp.py
+++ mailpimp.py
@@ -4,9 +4,18 @@ import email
 import sys
 import logging
 
+logging.basicConfig(filename="/var/log/mailpimp.log", level=logging.DEBUG)
 logger = logging.getLogger('mailpimp')
 
-msg = email.message_from_binary_file(sys.stdin.buffer)
-
-logger.info(msg)
-logger.info(dir(msg))
+try:
+    logger.info("trying to read mail")
+    sender = sys.argv[0]
+    recipient = sys.argv[1]
+    msg = email.message_from_binary_file(sys.stdin.buffer)
+    
+    logger.info(sender, recipient)
+    logger.info(msg)
+    logger.info(dir(msg))
+except Exception as e:
+    logger.exception(e)
+    sys.exit(1)
